9289405cfd97eba526ebb42e14f512e3efe1d6eb
thekaroe-thailand <thekaroe@gmail.com>
2024-10-11 02:48:23 +0700

    billSale

--- src/app/bill-sale/bill-sale.component.ts
+++ src/app/bill-sale/bill-sale.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import dayjs from 'dayjs';
 import config from '../../config';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-bill-sale',
@@ -32,6 +33,32 @@ export class BillSaleComponent {
     this.http.post(config.apiServer + '/api/billSale/list', payload).subscribe((res: any) => {
       this.billSales = res.results;
     });
+  }
+
+  async removeBillSale(id: number) {
+    // confirm by SWal
+    const button = await Swal.fire({
+      title: 'คุณต้องการยกเลิกใบเสร็จนี้ใช่หรือไม่?',
+      icon: 'warning',
+      showCancelButton: true,
+      showConfirmButton: true,
+    });
+
+    if (button.isConfirmed) {
+      this.http.delete(config.apiServer + '/api/billSale/remove/' + id).subscribe((res: any) => {
+        this.fetchData();
+      });
+    }
+  }
+
+  getPayType(payType: string) {
+    if (payType == 'cash') {
+      return 'เงินสด';
+    } else if (payType == 'transfer') {
+      return 'จ่ายด้วยบัญชีธนาคาร';
+    } else {
+      return '';
+    }
   }
 }
 
